cb7b7ef85f67e52859ae6650bc3ef220f5f64e16
pjs168686 <91727126+pjs168686@users.noreply.github.com>
2021-10-22 04:31:37 +0900

    Add files via upload

---
+++ cass/js/svg.js
@@ -0,0 +1,4 @@
+new Vivus('Layer_14',{type:'oneByOne',duration:200});
+new Vivus('Layer_13',{type:'oneByOne',duration:200});
+new Vivus('Layer_8',{type:'oneByOne',duration:200});
+new Vivus('Layer_5',{type:'oneByOne',duration:200});
